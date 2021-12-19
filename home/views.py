f4144e8d4df8d9ef0d71f8b347a4eb326d9d490d
Durmuş Ali Öztürk <durmusali.dali@gmail.com>
2021-12-19 23:35:46 +0300

    Birazcık dinamik Anasayfa Yapısı

--- home/views.py
+++ home/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home_view(request):
-    return render(request, "home.html",{})
+    if request.user.is_authenticated():
+        context = {"name":"Ali"}
+        return render(request, "home.html",context)
+    else:
+        context = {"name":"Misafir"}
+        return render(request, "home.html",context)
