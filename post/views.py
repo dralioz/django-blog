084cf48c37fe27db4bfc0d9e47bc04e062b70336
Durmuş Ali Öztürk <durmusali.dali@gmail.com>
2021-12-19 21:04:17 +0300

    Post yapısının her sayfası için html oluşturuldu.

--- post/views.py
+++ post/views.py
@@ -4,17 +4,17 @@ from django.shortcuts import render, HttpResponse
 
 # Her bir sayfanın html dosyası oluşturulacak.
 
-def post_index(requset):
-    return HttpResponse("<b>Post Indeks Sayfası</b>")
+def post_index(request):
+    return render(request, "post/index.html",{})
 
-def post_detail(requset):
-    return HttpResponse("<b>Post Detay Sayfası</b>")
+def post_detail(request):
+    return render(request, "post/detail.html",{})
 
-def post_create(requset):
-    return HttpResponse("<b>Post Oluşturma Sayfası</b>")
+def post_create(request):
+    return render(request, "post/create.html",{})
 
-def post_update(requset):
-    return HttpResponse("<b>Post Güncelleme Sayfası</b>")
+def post_update(request):
+    return render(request, "post/update.html",{})
 
-def post_delete(requset):
-    return HttpResponse("<b>Post Silme Sayfası</b>")
+def post_delete(request):
+    return render(request, "post/delete.html",{})
