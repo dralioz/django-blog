476218e0916d8ea6bc5f19e5e63fe9d5124ac599
Durmuş Ali Öztürk <durmusali.dali@gmail.com>
2021-12-20 14:46:49 +0300

    Dinamik index ve Detail

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home_view(request):
-    if request.user.is_authenticated():
+    if request.user.is_authenticated:
         context = {"name":"Ali"}
         return render(request, "home.html",context)
     else:
--- post/views.py
+++ post/views.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import get_object_or_404, render, HttpResponse
+from .models import Post
 
 # Create your views here.
 
 # Her bir sayfanın html dosyası oluşturulacak.
 
 def post_index(request):
-    return render(request, "post/index.html",{})
+    posts = Post.objects.all()
+    return render(request, "post/index.html",{"posts":posts})
 
-def post_detail(request):
-    return render(request, "post/detail.html",{})
+def post_detail(request, id):
+    post = get_object_or_404(Post, id = id)
+    context = {"post":post}
+    return render(request, "post/detail.html",context)
 
 def post_create(request):
     return render(request, "post/create.html",{})
