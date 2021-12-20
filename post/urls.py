b6af13a43ee810a1fe38b943a8a34a2cf4dedd47
Durmuş Ali Öztürk <durmusali.dali@gmail.com>
2021-12-20 17:33:07 +0300

    En dinamik yapı - index,detail

--- post/urls.py
+++ post/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import *
 
+global app_name 
+app_name = "post"
+
 urlpatterns = [
     path("index/", post_index),
-    path("<int:id>", post_detail),
+    path("<int:id>", post_detail, name = "detail"),
     path("create/", post_create),
     path("update/", post_update),
     path("delete/", post_delete),
